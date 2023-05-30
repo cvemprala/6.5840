313f2a8e22b930efe8985c078b5c88ea307c2716
Chaitanya Vemprala <chaitanya.vemprala@picktrace.com>
2023-05-30 00:24:59 -0700

    Completed the coordinator implementation. The coordinator is responsible for managing tasks, assigning them to workers, and keeping track of their status

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,149 @@
 package mr
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
 import "net"
 import "os"
 import "net/rpc"
 import "net/http"
 
+type TaskStatus int
 
-type Coordinator struct {
-	// Your definitions here.
+const (
+	Idle TaskStatus = iota
+	Wait
+	InProgress
+	Completed
+	Exit
+)
 
+type TaskType int
+
+const (
+	TaskMap TaskType = iota
+	TaskReduce
+)
+
+type State int
+
+const (
+	Map State = iota
+	Reduce
+	Done
+)
+
+type Task struct {
+	ID         int
+	Timestamp  time.Time
+	InputFile  string
+	TaskStatus TaskStatus
+	TaskType   TaskType
+	WorkerID   int
+	NReduce    int
 }
 
-// Your code here -- RPC handlers for the worker to call.
+type Coordinator struct {
+	mu          *sync.RWMutex
+	tasks       map[string]*Task
+	mapTasks    chan *Task
+	mTaskCount  int
+	reduceTasks chan *Task
+	rTaskCount  int
+	state       State
+	nReduce     int
+}
 
-//
-// an example RPC handler.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+func (c *Coordinator) GetTask(args *GetTaskArgs, reply *GetTaskReply) error {
+	task := c.AssignTask(args.WorkerID)
+	reply.Task = task
+	if task.TaskStatus == InProgress {
+		go c.checkTimeout(fmt.Sprintf("%d-%d", task.TaskType, task.ID))
+	}
 	return nil
 }
 
+func (c *Coordinator) MarkTask(args *ReportTaskArgs, reply *ReportTaskReply) error {
+	reply.OK = c.MarkTaskCompleted(args.ID, args.TaskType, args.WorkerID)
+	return nil
+}
 
-//
-// start a thread that listens for RPCs from worker.go
-//
+func (c *Coordinator) checkTimeout(taskID string) {
+	time.Sleep(10 * time.Second)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	task := c.tasks[taskID]
+	if task.TaskStatus == InProgress {
+		task.InputFile = ""
+		task.TaskStatus = Idle
+		task.WorkerID = -1
+		task.Timestamp = time.Time{}
+		if task.TaskType == TaskMap {
+			c.mapTasks <- task
+		} else {
+			c.reduceTasks <- task
+		}
+	}
+}
+
+func (c *Coordinator) MarkTaskCompleted(taskID int, taskType TaskType, workerID int) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	id := fmt.Sprintf("%s-%d", "Map", taskID)
+	if taskType == TaskReduce {
+		id = fmt.Sprintf("%s-%d", "Reduce", taskID)
+	}
+	task := c.tasks[id]
+	if task.WorkerID == workerID {
+		task.TaskStatus = Completed
+		if task.TaskType == TaskMap {
+			c.mTaskCount--
+		} else {
+			c.rTaskCount--
+		}
+		if c.mTaskCount == 0 && c.rTaskCount == 0 {
+			c.state = Done
+		}
+		return true
+	}
+	return false
+}
+
+func (c *Coordinator) AssignTask(workerID int) *Task {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.state == Done {
+		return &Task{TaskStatus: Exit}
+	} else if c.mTaskCount > 0 {
+		select {
+		case task := <-c.mapTasks:
+			task.TaskStatus = InProgress
+			task.Timestamp = time.Now()
+			task.WorkerID = workerID
+			c.tasks[fmt.Sprintf("Map-%d", task.ID)] = task
+			return task
+		default:
+			return &Task{TaskStatus: Wait}
+		}
+	} else if c.rTaskCount > 0 {
+		select {
+		case task := <-c.reduceTasks:
+			task.TaskStatus = InProgress
+			task.Timestamp = time.Now()
+			task.WorkerID = workerID
+			c.tasks[fmt.Sprintf("Reduce-%d", task.ID)] = task
+			return task
+		default:
+			return &Task{TaskStatus: Wait}
+		}
+	} else {
+		return &Task{TaskStatus: Wait}
+	}
+}
+
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +157,52 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
-// main/mrcoordinator.go calls Done() periodically to find out
-// if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
-	ret := false
-
-	// Your code here.
-
-
-	return ret
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.state == Done
 }
 
-//
-// create a Coordinator.
-// main/mrcoordinator.go calls this function.
-// nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
-	c := Coordinator{}
+	mapTasks := make(chan *Task, len(files))
+	reduceTasks := make(chan *Task, nReduce)
+	taskMap := make(map[string]*Task)
+	for index, file := range files {
+		task := &Task{
+			ID:         index,
+			InputFile:  file,
+			TaskStatus: Idle,
+			TaskType:   TaskMap,
+			WorkerID:   -1,
+			NReduce:    nReduce,
+		}
+		mapTasks <- task
+		taskMap[fmt.Sprintf("Map-%d", index)] = task
+	}
 
-	// Your code here.
+	for index := 0; index < nReduce; index++ {
+		task := &Task{
+			ID:         index,
+			InputFile:  "",
+			TaskStatus: Idle,
+			TaskType:   TaskReduce,
+			WorkerID:   -1,
+			NReduce:    nReduce,
+		}
+		reduceTasks <- task
+		taskMap[fmt.Sprintf("Reduce-%d", index)] = task
+	}
 
+	c := Coordinator{
+		tasks:       taskMap,
+		mapTasks:    mapTasks,
+		mTaskCount:  len(files),
+		reduceTasks: reduceTasks,
+		rTaskCount:  nReduce,
+		state:       Map,
+		nReduce:     nReduce,
+		mu:          new(sync.RWMutex),
+	}
 
 	c.server()
 	return &c
